aa32b48872013f10e82b3cd219888cf7e04fcbc5
Payman <payman.hoseini7@gmail.com>
2024-09-16 17:15:00 +0430

    destination page 70%

---
+++ src/app/components/desHero.tsx
@@ -0,0 +1,60 @@
+'use client'
+import {  useRef, useState } from "react";
+import { PageFile } from "./contracts";
+
+
+export default function DesHero({ File } : {File : PageFile}){
+    const btnMoonRef = useRef(null)
+    const btnMarsRef = useRef(null)
+    const btnEurRef = useRef(null)
+    const btnTitanRef = useRef(null)
+    let [ desValue , setDesValue ] = useState(File.destinations[0])
+    function destSelector(e : React.MouseEvent<HTMLButtonElement, MouseEvent>){
+        if(e.target == btnMoonRef.current){
+            setDesValue(File.destinations[0])
+        }
+        else if(e.target == btnMarsRef.current) {
+            setDesValue(File.destinations[1])
+        }
+        else if(e.target == btnEurRef.current) {
+            setDesValue(File.destinations[2])
+        }
+        else {
+            setDesValue(File.destinations[3])
+        }
+        
+    }
+    return (
+        <div className="mx-40 mb-12 h-[70%]">
+            <div className="grid grid-cols-2 gap-x-8 h-full  place-content-center">
+                <div>
+                    <img src={desValue.images.png} alt="moon"/>
+                </div>
+                <div>
+                    <div className="flex">
+                        <button ref={btnMoonRef} onClick={destSelector} className="text-white font-Barlow-Condensed tracking-[2px] pb-2 mr-8 border-b-4 border-b-transparent hover:border-b-white/45 duration-700">MOON</button>
+                        <button ref={btnMarsRef} onClick={destSelector} className="text-white font-Barlow-Condensed tracking-[2px] pb-2 mr-8 border-b-4 border-b-transparent hover:border-b-white/45 duration-700">MARS</button>
+                        <button ref={btnEurRef} onClick={destSelector} className="text-white font-Barlow-Condensed tracking-[2px] pb-2 mr-8 border-b-4 border-b-transparent hover:border-b-white/45 duration-700">EUROPA</button>
+                        <button ref={btnTitanRef} onClick={destSelector} className="text-white font-Barlow-Condensed tracking-[2px] pb-2 border-b-4 border-b-transparent hover:border-b-white/45 duration-700">TITAN</button>
+                    </div>
+                    <div>
+                        <div className="py-10 border-b border-b-white/30 w-[80%]">
+                            <h2 className="text-[100px] leading-normal font-Bellefair text-white">{desValue.name.toUpperCase()}</h2>
+                            <p className="text-lg text-Blue-300 font-Barlow text-balance leading-[1.8]">{desValue.description}</p>
+                        </div>
+                        <div className="mt-10 flex w-full space-x-6">
+                            <div className="basis-1/2">
+                                <p className="font-Barlow-Condensed text-sm tracking-[2px] text-Blue-300">AVG. DISTANCE</p>
+                                <p className="mt-3 text-white font-Bellefair lead text-[28px]">{desValue.distance.toUpperCase()}</p>
+                            </div>
+                            <div className="basis-1/2">
+                                <p className="font-Barlow-Condensed text-sm tracking-[2px] text-Blue-300">EST. TRAVEL TIME</p>
+                                <p className="mt-3 text-white font-Bellefair lead text-[28px]">{desValue.travel.toUpperCase()}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
+
 import Header from "../components/header";
 import { promises as fs } from 'fs';
 import { PageFile } from "../components/contracts";
-import { json } from "stream/consumers";
+import DesHero from "../components/desHero";
 
 export default async function Destination() {
     const file = await fs.readFile(process.cwd() + '/src/data.json' , 'utf8');
@@ -19,38 +19,7 @@ export default async function Destination() {
                     <h2 className="font-Barlow-Condensed text-[28px] tracking-[4px] text-white ml-6">PICK YOUR DESTINATION</h2>
                 </div>
             </div>
-            <div className="mx-40 mb-12 h-[70%]">
-                <div className="grid grid-cols-2 gap-x-8 h-full  place-content-center">
-                    <div>
-                        <img src={File.destinations[0].images.png} alt="moon"/>
-                    </div>
-                    <div>
-                        <div className="flex">
-                            <button className="text-white font-Barlow-Condensed tracking-[2px] pb-2 mr-8 border-b-4 border-b-transparent hover:border-b-white/45 duration-700">MOON</button>
-                            <button className="text-white font-Barlow-Condensed tracking-[2px] pb-2 mr-8 border-b-4 border-b-transparent hover:border-b-white/45 duration-700">MARS</button>
-                            <button className="text-white font-Barlow-Condensed tracking-[2px] pb-2 mr-8 border-b-4 border-b-transparent hover:border-b-white/45 duration-700">EUROPA</button>
-                            <button className="text-white font-Barlow-Condensed tracking-[2px] pb-2 border-b-4 border-b-transparent hover:border-b-white/45 duration-700">TITAN</button>
-                        </div>
-                        <div>
-                            <div className="py-10 border-b border-b-white/30 w-[80%]">
-                                <h2 className="text-[100px] leading-normal font-Bellefair text-white">MOON</h2>
-                                <p className="text-lg text-Blue-300 font-Barlow text-balance leading-[1.8]">See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back
-                                refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
-                            </div>
-                            <div className="mt-10 flex w-full space-x-6">
-                                <div className="basis-1/2">
-                                    <p className="font-Barlow-Condensed text-sm tracking-[2px] text-Blue-300">AVG. DISTANCE</p>
-                                    <p className="mt-3 text-white font-Bellefair lead text-[28px]">384,400 KM</p>
-                                </div>
-                                <div className="basis-1/2">
-                                    <p className="font-Barlow-Condensed text-sm tracking-[2px] text-Blue-300">EST. TRAVEL TIME</p>
-                                    <p className="mt-3 text-white font-Bellefair lead text-[28px]">3 DAYS</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <DesHero File={File}/>
         </div>
       </div>
     </>
