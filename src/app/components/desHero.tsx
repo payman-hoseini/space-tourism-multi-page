15ac828d51961caaf3d57aceb373f549dd8321b8
Payman <payman.hoseini7@gmail.com>
2024-09-18 23:25:54 +0430

    responsive destination page for mobile 90%

--- src/app/components/desHero.tsx
+++ src/app/components/desHero.tsx
@@ -25,24 +25,24 @@ export default function DesHero({ File } : {File : PageFile}){
         
     }
     return (
-        <div className="mx-40 mt-6 lg:mb-12 md:mb-10 h-[70%]">
-            <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-8 lg:h-full  place-content-center">
-                <div className="my-5">
+        <div className="md:mx-40 mx-6 mt-6 lg:mb-12 md:mb-10 h-[70%]">
+            <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-8 lg:h-full  place-content-center place-items-center md:place-items-stretch">
+                <div className="md:my-5 w-1/2 md:w-full">
                     <img src={desValue.images.png} alt={desValue.name}/>
                 </div>
-                <div className="md:mt-8 lg:mt-0 md:flex md:flex-col md:items-center lg:block">
+                <div className="mt-8 lg:mt-0 flex flex-col items-center lg:block">
                     <div className="flex">
-                        <button ref={btnMoonRef} onClick={destSelector} className={`text-white font-Barlow-Condensed tracking-[2px] pb-2 mr-8 border-b-4 border-b-transparent hover:border-b-white/45 duration-700 ${desValue.name == 'Moon' ? "border-b-white" : ""}`}>MOON</button>
-                        <button ref={btnMarsRef} onClick={destSelector} className={`text-white font-Barlow-Condensed tracking-[2px] pb-2 mr-8 border-b-4 border-b-transparent hover:border-b-white/45 duration-700 ${desValue.name == 'Mars' ? "border-b-white" : ""}`}>MARS</button>
-                        <button ref={btnEurRef} onClick={destSelector} className={`text-white font-Barlow-Condensed tracking-[2px] pb-2 mr-8 border-b-4 border-b-transparent hover:border-b-white/45 duration-700 ${desValue.name == 'Europa' ? "border-b-white" : ""}`}>EUROPA</button>
-                        <button ref={btnTitanRef} onClick={destSelector} className={`text-white font-Barlow-Condensed tracking-[2px] pb-2 border-b-4 border-b-transparent hover:border-b-white/45 duration-700 ${desValue.name == 'Titan' ? "border-b-white" : ""}`}>TITAN</button>
+                        <button ref={btnMoonRef} onClick={destSelector} className={`text-white md:text-base text-[14px] font-Barlow-Condensed tracking-[2px] pb-2 mr-8 border-b-4 border-b-transparent hover:border-b-white/45 duration-700 ${desValue.name == 'Moon' ? "border-b-white" : ""}`}>MOON</button>
+                        <button ref={btnMarsRef} onClick={destSelector} className={`text-white md:text-base text-[14px] font-Barlow-Condensed tracking-[2px] pb-2 mr-8 border-b-4 border-b-transparent hover:border-b-white/45 duration-700 ${desValue.name == 'Mars' ? "border-b-white" : ""}`}>MARS</button>
+                        <button ref={btnEurRef} onClick={destSelector} className={`text-white md:text-base text-[14px] font-Barlow-Condensed tracking-[2px] pb-2 mr-8 border-b-4 border-b-transparent hover:border-b-white/45 duration-700 ${desValue.name == 'Europa' ? "border-b-white" : ""}`}>EUROPA</button>
+                        <button ref={btnTitanRef} onClick={destSelector} className={`text-white md:text-base text-[14px] font-Barlow-Condensed tracking-[2px] pb-2 border-b-4 border-b-transparent hover:border-b-white/45 duration-700 ${desValue.name == 'Titan' ? "border-b-white" : ""}`}>TITAN</button>
                     </div>
-                    <div className="md:text-center lg:text-start">
+                    <div className="text-center lg:text-start">
                         <div className="lg:py-10 py-6 border-b border-b-white/30 lg:w-[80%]">
-                            <h2 className="text-[100px] leading-normal font-Bellefair text-white">{desValue.name.toUpperCase()}</h2>
-                            <p className="text-lg text-Blue-300 font-Barlow text-balance leading-[1.8]">{desValue.description}</p>
+                            <h2 className="lg:text-[100px] md:text-[80px] text-[56px] leading-normal font-Bellefair text-white">{desValue.name.toUpperCase()}</h2>
+                            <p className="md:text-lg text-base text-Blue-300 font-Barlow text-balance leading-[1.8]">{desValue.description}</p>
                         </div>
-                        <div className="lg:mt-10 md:mt-6 flex w-full space-x-6">
+                        <div className="lg:mt-10 mt-6 flex md:flex-row flex-col w-full md:space-x-6 space-y-6 md:space-y-0">
                             <div className="basis-1/2">
                                 <p className="font-Barlow-Condensed text-sm tracking-[2px] text-Blue-300">AVG. DISTANCE</p>
                                 <p className="mt-3 text-white font-Bellefair lead text-[28px]">{desValue.distance.toUpperCase()}</p>
--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -12,10 +12,10 @@ export default async function Destination() {
       <div className="h-screen bg-Blue-900">
         <div className="destinationBackground h-screen mx-auto max-w-[1440px] transitionPage">
             <Header />
-            <div className="lg:mx-40 md:mx-10 lg:mt-12 md:mt-10">
-                <div className="flex items-center">
-                    <span className="font-Barlow-Condensed text-white/20 font-bold text-[28px] tracking-[4.72px]">01</span>
-                    <h2 className="font-Barlow-Condensed text-[28px] tracking-[4px] text-white ml-6">PICK YOUR DESTINATION</h2>
+            <div className="lg:mx-40 md:mx-10 lg:mt-12 md:mt-10 mt-6">
+                <div className="flex items-center md:justify-start justify-center">
+                    <span className="font-Barlow-Condensed text-white/20 font-bold md:text-[28px] text-base tracking-[4.72px]">01</span>
+                    <h2 className="font-Barlow-Condensed md:text-[28px] text-base tracking-[4px] text-white ml-6">PICK YOUR DESTINATION</h2>
                 </div>
             </div>
             <DesHero File={File}/>
