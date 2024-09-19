fafde1f19c56c80680c323e5a6b86781575a6ea5
Payman <payman.hoseini7@gmail.com>
2024-09-19 12:41:01 +0430

    responsive technology page 90%

--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -11,10 +11,10 @@ export default async function Crew(){
             <div className="h-screen bg-Blue-900">
                 <div className="techBackground h-screen mx-auto max-w-[1440px] transitionPage">
                     <Header />
-                    <div className="lg:ml-40 md:ml-10 md: mt-12">
-                        <div className="flex items-center">
-                            <span className="font-Barlow-Condensed text-white/20 font-bold text-[28px] tracking-[4.72px]">03</span>
-                            <h2 className="font-Barlow-Condensed text-[28px] tracking-[4px] text-white ml-6">SPACE LAUNCH 101</h2>
+                    <div className="lg:ml-40 md:ml-10 md:mt-10 mt-6">
+                        <div className="flex items-center justify-center md:justify-start">
+                            <span className="font-Barlow-Condensed text-white/20 font-bold lg:text-[28px] md:text-xl text-base tracking-[4.72px]">03</span>
+                            <h2 className="font-Barlow-Condensed lg:text-[28px] md:text-xl text-base tracking-[4px] text-white ml-6">SPACE LAUNCH 101</h2>
                         </div>
                     </div>
                     <TechHero File={File} />
